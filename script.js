477820399f5dd6308ac4b8041346e3f00dd17f1d
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-05 19:25:26 +0300

    Refactored books data to use class instead

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const form = document.querySelector('.form');
-const list = document.querySelector('.list');
-let books = JSON.parse(localStorage.getItem('Books')) || [];
-function storeBooks() {
-  localStorage.setItem('Books', JSON.stringify(books));
+class Books {
+  bookList = [];
+
+  constructor() {
+    this.bookList = JSON.parse(localStorage.getItem('Books')) || [];
+  }
+
+  addBook(book) {
+    this.bookList.push(book);
+    this.storeList();
+  }
+
+  removeBook(id) {
+    this.bookList = this.bookList.filter((book) => book.id !== id);
+    this.storeList();
+  }
+
+  storeList() {
+    localStorage.setItem('Books', JSON.stringify(this.bookList));
+  }
 }
+const books = new Books();
+const list = document.querySelector('.list');
+const form = document.querySelector('.form');
 function displayBooks() {
   list.innerHTML = '';
   const fragment = new DocumentFragment();
-  books.forEach((book) => {
+  books.bookList.forEach((book) => {
     const item = document.createElement('li');
     let itemHTML = `<p>"${book.title}" by ${book.author}</p>`;
     itemHTML += `<button id="${book.id}">Remove</button>`;
@@ -15,24 +33,19 @@ function displayBooks() {
     fragment.appendChild(item);
   });
   list.appendChild(fragment);
-  storeBooks();
 }
-function addBook(event) {
+function addEvent(event) {
   event.preventDefault();
-  books.push({
-    id: `${Date.now()}`,
-    title: form.title.value,
-    author: form.author.value,
-  });
+  books.addBook({ id: `${Date.now()}`, title: form.title.value, author: form.author.value });
   displayBooks();
   form.reset();
 }
-function removeBook(event) {
+function removeEvent(event) {
   if (event.target.id !== '') {
-    books = books.filter((book) => book.id !== event.target.id);
+    books.removeBook(event.target.id);
     displayBooks();
   }
 }
-list.addEventListener('click', removeBook);
-form.addEventListener('submit', addBook);
+list.addEventListener('click', removeEvent);
+form.addEventListener('submit', addEvent);
 displayBooks();
