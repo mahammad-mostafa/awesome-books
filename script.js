0cba7317db982355a7488df62a0d568e3bca7a26
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-06 22:16:59 +0300

    Implemented dynamic navigation events

--- script.js
+++ script.js
@@ -20,8 +20,25 @@ class Books {
   }
 }
 const books = new Books();
+const pages = ['#list', '#add', '#contact'];
+const menu = document.querySelector('.header-menu');
+const links = document.querySelectorAll('.header-menu a');
+const contents = document.querySelectorAll('.content');
 const list = document.querySelector('.content-list');
 const form = document.querySelector('.content-form');
+function displayContent(hash) {
+  pages.forEach((page) => {
+    const link = links[pages.indexOf(page)];
+    const content = contents[pages.indexOf(page)];
+    if (page === hash && content.classList.contains('visible') === false) {
+      link.classList.add('active');
+      content.classList.add('visible');
+    } else if (content.classList.contains('visible')) {
+      link.classList.remove('active');
+      content.classList.remove('visible');
+    }
+  });
+}
 function displayBooks() {
   list.innerHTML = '';
   const fragment = new DocumentFragment();
@@ -33,6 +50,12 @@ function displayBooks() {
     fragment.appendChild(item);
   });
   list.appendChild(fragment);
+}
+function contentEvent(event) {
+  event.preventDefault();
+  if (event.target.hash !== '') {
+    displayContent(event.target.hash);
+  }
 }
 function addEvent(event) {
   event.preventDefault();
@@ -46,6 +69,8 @@ function removeEvent(event) {
     displayBooks();
   }
 }
+menu.addEventListener('click', contentEvent);
 list.addEventListener('click', removeEvent);
 form.addEventListener('submit', addEvent);
+displayContent('#list');
 displayBooks();
